fbe8e7d69bb21140c4889bac805fdbc3c1b3c865
Ayushi Sahu <ayushisahu415@gmail.com>
2020-12-17 20:00:53 +0530

    first commit

---
+++ modal.js
@@ -0,0 +1,9 @@
+var button =document.getElementById('btn');
+var modal =document.getElementById('mymodal');
+
+ button.addEventListener('click',function(){
+   modal.style.display='initial';
+//    button.style.display='none';
+ });
+
+ 
